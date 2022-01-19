dfa4a2c6b42024b4beed5ce788adf3e14e901e6e
emilio kariuki <70817006+emiliokariuki@users.noreply.github.com>
2022-01-19 21:25:52 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,33 @@ import 'package:flutter/material.dart';
 import 'package:contactus/contactus.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: ContactUs(
-          logo: AssetImage('images/crop.jpg'),
-          email: 'emilio113kariuki@gmail.com',
-          companyName: 'Ecoville',
-          phoneNumber: '+254796250443',
-          dividerThickness: 2,
-          website: 'https://abhishekdoshi.godaddysites.com',
-          githubUserName: '@emiliokariuki',
-          linkedinURL: 'https://www.linkedin.com/in/abhishek-doshi-520983199/',
-          tagLine: 'Flutter Developer',
-          twitterHandle: '@HighTech113',
-          // instagramUserName: '_emiliokariuki', 
-          taglineColor: Colors.amber, 
-          textColor: Colors.black, 
-          companyColor: Colors.red, 
-          cardColor: Colors.blueGrey,
-      ),
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey.shade900,
+          body: ContactUs(
+            logo: const AssetImage("assets/coode.jpg"),
+            email: 'emilio113kariuki@gmail.com',
+            companyName: 'Ecoville',
+            phoneNumber: '+254796250443',
+            dividerThickness: 2,
+            website: 'https://emilio-kariuki.com',
+            githubUserName: '@emiliokariuki',
+            linkedinURL: 'https://www.linkedin.com/in/emilio-kariuki',
+            tagLine: 'Android Developer || Android Lead',
+            twitterHandle: '@HighTech113',
+            // instagramUserName: '_emiliokariuki',
+            taglineColor: Colors.blueGrey.shade200,
+            textColor: Colors.black,
+            companyColor: Colors.red,
+            cardColor: Colors.blueGrey.shade200,
+          ),
+        ));
   }
 }
